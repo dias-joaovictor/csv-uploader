5ffa58303b321a4da9a8881ef19882d9737fe1ae
João Victor Dias <joao.dias@bestseller.com>
2023-10-29 12:03:25 +0100

    Remove unnecessary code

--- src/main/java/com/gerimedica/csvuploader/endpoint/MedicalDataEndpoint.java
+++ src/main/java/com/gerimedica/csvuploader/endpoint/MedicalDataEndpoint.java
@@ -16,7 +16,6 @@ import com.gerimedica.csvuploader.repository.MedicalDataRepository;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.List;
 
 @Slf4j
 @RequestMapping("/medical-data")
